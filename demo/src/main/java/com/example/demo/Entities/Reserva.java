2ccba94c0deb000b80fe7bc60275b7959931213f
gguilhermeazevedo <gguilhermeazvdo@gmail.com>
2025-05-08 22:04:37 -0300

    Entidade Pagamento e Reserva add

---
+++ demo/src/main/java/com/example/demo/Entities/Reserva.java
@@ -0,0 +1,46 @@
+package com.example.demo.Entities;
+
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Entity
+@Table(name = "reservas")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Reserva {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long clienteId;
+
+    @Column(nullable = false, unique = true)
+    private Long carroId;
+
+    @Column(nullable = false, unique = true)
+    private LocalDateTime dataInicio;
+
+    @Column(nullable = false)
+    private LocalDateTime dataFim;
+
+    @Column(nullable = false)
+    private String Status;
+
+    
+}
+
