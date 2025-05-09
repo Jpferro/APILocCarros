0d9a3bfe4b6e455a9c9395b3e759be98e0e9a7a6
Jpferro <joaoferromga1508@gmail.com>
2025-05-09 08:42:16 -0300

    add manutencao e clientes

--- demo/src/main/java/com/example/demo/Entities/Manutencao.java
+++ demo/src/main/java/com/example/demo/Entities/Manutencao.java
@@ -2,11 +2,15 @@ package com.example.demo.Entities;
 
 import java.time.LocalDate;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,27 +20,30 @@ import lombok.Setter;
 
 @Data
 @Entity
-@Table(name = "clientes")
+@Table(name = "manutencao")
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
 
-public class Cliente {
-    @Id
+public class Manutencao {
+
+ @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "carros") 
     @Column(nullable = false)
-    private String nome;
+    private Long carroId;
 
     @Column(nullable = false, unique = true)
-    private String documento;
+    private String desc;
 
     @Column(nullable = false, unique = true)
-    private String email;
+    private String status;
 
     @Column(nullable = false)
-    private String endereco;
+    private LocalDateTime dataInicio;
 
     @Column(nullable = false)
-    private String telefone;
+    private LocalDateTime dataFim;
 }
