185a2667c79d42d766c0316f59412d7d5dc31a35
gguilhermeazevedo <168605239+gguilhermeazevedo@users.noreply.github.com>
2025-05-28 19:45:51 -0300

    Update CarroRepository.java

--- demo/src/main/java/com/example/demo/repository/CarroRepository.java
+++ demo/src/main/java/com/example/demo/repository/CarroRepository.java
@@ -1,5 +1,13 @@
-package com.example.demo.repository;
+package com.example.demo.Repositories;
 
-public class CarroRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.Entities.Carro;
+
+public interface CarroRepository extends JpaRepository<Carro, Long> {
+    
+    
+    List<Carro> findByDisponivelTrue();
 }
