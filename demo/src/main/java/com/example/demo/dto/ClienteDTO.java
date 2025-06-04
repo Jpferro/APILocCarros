925f7a31d9d562c88d54aac58916a292148c04e3
daudt0017 <gkdaudt@gmail.com>
2025-06-04 19:51:56 -0300

    Fix

--- demo/src/main/java/com/example/demo/dto/ClienteDTO.java
+++ demo/src/main/java/com/example/demo/dto/ClienteDTO.java
@@ -1,5 +1,12 @@
 package com.example.demo.dto;
 
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
 public class ClienteDTO {
 
   private Long id;
